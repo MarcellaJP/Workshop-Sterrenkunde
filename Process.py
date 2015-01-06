af9791b4cf83ea3b2bc2d7a3167eacdc27c9a3fa
MarcellaJP <marcella.wijngaarden@student.uva.nl>
2015-01-06 13:13:43 +0100

    Unconfirmed planets added - code working for new database file

--- Process.py
+++ Process.py
@@ -2,16 +2,11 @@ __author__ = 'Marcella Wijngaarden & Joris Schefold'
 import csv
 import numpy as np
 import matplotlib.pyplot as plt
-import os
-import sys
-import time
 
 # FILE = "planets_withPandM.csv"
-FILE = "planets.csv"
 # FILE = "planets.csv"
+FILE = "cumulative.csv"
 FILE_NAME = "Data\\" + FILE
-
-
 
 BINS = 60
 G = float(6.67384) * float((10**(-11)))  # Gravitational constant in  m^3 / (kg * s^2)
@@ -29,7 +24,7 @@ def readData():
     combination = []
     counter = 0
     weighted_occurences = {}
-
+    weighted_occurences_data = {}
     # Skip header lines
     row = data_reader.next()
     while row[0][0] == '#':
@@ -38,23 +33,28 @@ def readData():
 
     koude_planeten = 0
     for row in data_reader:
-        count += 1
-        # Set variables to be retrieved
-        distance = row[label_row.index("st_dist")]      # Distance (PC)
-        T_star = row[label_row.index("st_teff")]        # T* (K)
-        R_star = row[label_row.index("st_rad")]        # R* (R_solar)
-        M_star = row[label_row.index("st_mass")]        # M* (M_solar)
-        period = float(row[label_row.index("pl_orbper")])         # P (days)
-        R_planet = row[label_row.index("pl_radj")]         # Planet radius in Jupiter Radius
-        gravitation = row[label_row.index("st_logg")]      # Stellar surface gravitation in Log10(cm/s^2)
-        e = row[label_row.index("pl_orbeccen")]             # Orbital eccentricity
-        discovery_method = row[label_row.index("pl_discmethod")]
+        # if row[label_row.index("koi_disposition")] != "CONFIRMED":
+        #     continue
 
-        if discovery_method != "Transit":
-            continue
+        counter += 1
+
+        # Set variables to be retrieved
+        distance = row[label_row.index("koi_sma")]      # Distance (AU)
+        T_star = row[label_row.index("koi_steff")]        # T* (K)
+        R_star = row[label_row.index("koi_srad")]        # R* (R_solar)
+        M_star = row[label_row.index("koi_smass")]        # M* (M_solar)
+        period = float(row[label_row.index("koi_period")])         # P (days)
+        R_planet = row[label_row.index("koi_prad")]         # Planet radius in Earth Radius
+        T_planet_dat = row[label_row.index("koi_teq")]
+        gravitation = row[label_row.index("koi_slogg")]      # Stellar surface gravitation in Log10(cm/s^2)
+        e = row[label_row.index("koi_eccen")]             # Orbital eccentricity
+        nr_transits = row[label_row.index("koi_num_transits")]  # Number of transits
+        confirmed =  row[label_row.index("koi_disposition")]  # CONFIRMED / CANDIDATE
+
+        # if nr_transits > 0:
+        #     continue
 
         if M_star != "" and period != "":
-            # print "calculate distance"
             distance = getDistance(float(M_star), float(period))
         else:
             if R_planet != "":
@@ -66,9 +66,6 @@ def readData():
             print T_star, R_star, distance, "SKIP"
             continue
 
-        # Geometric detection correction
-        probability = solarRadiusToMeters(float(R_star))/parsecToMeters(float(distance))
-
         # Calculate planet temperature
         if e != "":
             T_planet = getPlanetTemperature(T_star, R_star, distance, float(e))
@@ -78,16 +75,22 @@ def readData():
             koude_planeten += 1
             print "cold planet"
             continue
+        if T_planet < 188:
+            print T_planet, period/365.25
 
         # Geometric detection correction
-        geometric_probability = solarRadiusToMeters(float(R_star))/parsecToMeters(float(distance))
+        geometric_probability = solarRadiusToMeters(float(R_star))/AUToMeters(float(distance))
 
         timing_probability = 1
         if 2 < MISSIONLENGTH / period < 3:
             time_period = MISSIONLENGTH % period  # Period in which a transit must occure
-            timing_probability = time_period / YEAR
+            timing_probability = time_period / period
 
         probability = geometric_probability * timing_probability
+
+        # Use Kepler Planet temperature if available
+        if T_planet_dat != "":
+            T_planet = int(float(T_planet_dat))
 
         # Weighted occurences are corrected planet occurences
         if T_planet in weighted_occurences:
@@ -95,58 +98,77 @@ def readData():
         else:
             weighted_occurences[T_planet] = 1/probability
 
+        # if T_planet_dat != "":
+        #     # print T_planet_dat, " and ", T_planet
+        #     if T_planet_dat in weighted_occurences_data:
+        #         weighted_occurences_data[T_planet_dat] += 1/probability
+        #     else:
+        #         weighted_occurences_data[T_planet_dat] = 1/probability
+
         combination.append([T_planet, probability])
         selected_data.append(T_planet)
         # R_star = float(solarRadiusToMeters(float(R_star_)))
         # distance = float(parsecToMeters(float(distance_)))
         # print distance, float(parsecToMeters(float(distance))), T_star, R_star, float(solarRadiusToMeters(float(R_star)))
 
-    print selected_data
+    # print weighted_occurences
     print len(combination)
-    print 'total datalines : ', count
+    print 'count = ', counter
+    print 'total datalines : ', counter
+    # print weighted_occurences
+    # print weighted_occurences_data
     return selected_data, combination, weighted_occurences
 
 
 def makeBarchart(weighted_occurences, N_bins):
     global FIGURE_COUNTER
-    weighted_occurences_list = [0 for _ in range(N_bins)]
     temp_list = weighted_occurences.keys()
-    starting_temp = 0
-    temp_range = 500 # max(temp_list) - starting_temp
-    temp_interval = temp_range / float(N_bins)
 
+    # Set plot range
+    plot_range = [0, 3000]    # [0, max(temp_list)
+
+    # Layout plot variables
+    tick_size = 500              # x-axis tick size 1000
     font = {'size': 18}
     plt.rc('font', **font)
 
+    weighted_occurences_list = [0 for _ in range(N_bins)]
+    planet_occurences_list = [0 for _ in range(N_bins)]
+    starting_temp = plot_range[0]
+    temp_range = plot_range[1] - plot_range[0]  #max(temp_list) - starting_temp
+    temp_interval = temp_range / float(N_bins)
+
     HZ_count = 0
     pl_count = 0
+
     for temperature in temp_list:
         if temperature <= temp_range:
+
             weighted_occurences_list[int((temperature - starting_temp)/float(temp_interval))] += weighted_occurences[temperature]
+            planet_occurences_list[int((temperature - starting_temp)/float(temp_interval))] += 1
+            # pl_count += 1
             pl_count += weighted_occurences[temperature]
             if temperature >= 273 and temperature <= 373:
                 HZ_count += weighted_occurences[temperature]
-                print temperature, 'HZ !!!!!!!!!!!!!!!!1'
         else:
             continue
 
     x_labels = []
-    tick_size = 50 # 1000
     x_positions = []
     temperature = starting_temp
-    while temperature < 500: # max(temp_list):
+    while temperature <= plot_range[1] and temperature >= plot_range[0]:  #max(temp_list):
         x_labels.append(temperature)
-        x_positions.append(temperature/float(temp_interval))
+        x_positions.append(temperature / float(temp_interval))
         temperature += tick_size
 
-
+    # print weighted_occurences_list
+    # print planet_occurences_list
     plt.figure(FIGURE_COUNTER)
     plt.title("Corrected Kepler planet distribution")
     plt.xlabel("Temperature (K)", fontsize='17')
     plt.ylabel("Number of planets", fontsize='17')
     FIGURE_COUNTER += 1
-    plt.bar(left=range(N_bins), height=weighted_occurences_list, label="Planets in HZ : " + str(int(round(HZ_count))) \
-                                                                       + " of " + str(int(round(pl_count))))
+    plt.bar(left=range(N_bins), height=weighted_occurences_list, label="Nr of planets: " + str(int(round(pl_count)))) # "Planets in HZ : " + str(int(round(HZ_count))) + " of " + str(int(round(pl_count))))
     plt.legend(loc='upper right', fontsize='17')
     plt.tight_layout()
     plt.xticks(x_positions, x_labels)
@@ -220,7 +242,7 @@ def massFromGravitation(g, r_planet):
     Returns planet mass in Solar Mass units.
     """
     g = (10**float(g)) / 100.  # Converts units from Log10(cm/s^2) to m/s^2
-    r_planet = jupiterRadiusToMeters(float(r_planet))
+    r_planet = earthRadiusToMeters(float(r_planet))
     return (g * (r_planet**2) / G) / SOLAR_MASS
 
 def solarMasstoKg(mass):
@@ -234,8 +256,14 @@ def daysToSeconds(days):
 def metersToParsec(meters):
     return float(meters) * 3.24077929 * (10**(-17))
 
+
+def earthRadiusToMeters(radius):
+    return float(radius) * 6371000
+
+
 def jupiterRadiusToMeters(radius):
     return float(radius) * 69911000
+
 
 def solarRadiusToMeters(radius):
     return float(radius) * 6.955*(10**8)
@@ -245,11 +273,18 @@ def parsecToMeters(parsec):
     return float(parsec) * 3.08567758 * (10**16)
 
 
+def metersToAU(meters):
+    return float(meters) / (1.49597870700 * 10**11)
+
+
+def AUToMeters(AU):
+    return float(AU) * 1.49597870700 * 10**11
+
 def getDistance(mass, period):
     """
     Use Kepler's third law to calculate the distance between planet and star given M_star and P_orbit
     """
-    return metersToParsec((G * solarMasstoKg(mass) * (daysToSeconds(period)**2) / (4 * PI**2))**(1/3.))
+    return metersToAU((G * solarMasstoKg(mass) * (daysToSeconds(period)**2) / (4 * PI**2))**(1/3.))
 
 
 def getPlanetTemperature(T_star_, R_star_, distance_, e=0, albedo=0):
@@ -259,13 +294,13 @@ def getPlanetTemperature(T_star_, R_star_, distance_, e=0, albedo=0):
     """
 
     R_star = float(solarRadiusToMeters(float(R_star_)))
-    distance = float(parsecToMeters(float(distance_)))
+    distance = float(AUToMeters(float(distance_)))
     T_star = float(T_star_)
-    # return T_star * numpy.sqrt(R_star/(2*distance))
+
     return ((1 - albedo)*(T_star**4)*(R_star**2)/(4 * (distance**2) * (1 - e)**(1/2.)))**(1/4.)
 
 
 data = readData()
 makeBarchart(data[2], BINS)
-makeHistogram(data[0], data[1])
+# makeHistogram(data[0], data[1])
 plt.show()
